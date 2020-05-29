ab033118fffb46323ecdd5040eec2daebe7a498f
Felipe Benevides Dell <felipebenevides.ts@gmail.com>
2020-05-28 23:22:02 -0300

    src:front

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import $ from "jquery";
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  public codiprod:any;
+  public prod:any = {"codi":"","desc":"", "codifab":"","qtd":"", "flag":false};
+  constructor() {
+  }
+  
+  clickedSearch(){
 
-  constructor() {}
+    $.get( "http://192.168.15.12/instaparts/oci.php?CODIPROD="+this.codiprod, ( data )=>{
+      data = JSON.parse(data);
+      if(!(data.data[0])){
+        this.prod.flag = false;
+        return;
+      }
+      data = data.data[0];
+      console.log(data);
+      this.prod.codi = data['CODI_PROD'];
+      this.prod.desc = data['DESC_PROD'];
+      this.prod.codifab = data['CODIFAB_PROD'];
+      this.prod.qtd = data['QTDD_QTDD'];
+      this.prod.flag = true;
+    });
+
+  }
+
+  clickedBarcode(){
+    alert('');
+  }
+  onSearchChange(event){
+    this.prod.flag = false;
+  }
+  selectfile(ident){
+    $("#input-"+ident+" input").trigger('click');
+  }
 
 }
