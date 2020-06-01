11324bbfd874a41fe59ccbfab6e597bad7bef1e5
Felipe Benevides Dell <felipebenevides.ts@gmail.com>
2020-05-31 23:36:09 -0300

    src:test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,12 +9,95 @@ import $ from "jquery";
 export class Tab1Page {
   public codiprod:any;
   public prod:any = {"codi":"","desc":"", "codifab":"","qtd":"", "flag":false};
+  public image:any = {"flag":false};
+  public origin:any = origin;
   constructor() {
+    // this.codiprod = '010278';
+    this.clickedSearch();
   }
-  
-  clickedSearch(){
+  clickedBarcode(){
+    alert('');
+  }
+  onSearchChange(event){
+    this.prod.flag = false;
+  }
+  selectfile(ident){
+    $("#input-"+ident+" input").trigger('click');
+  }
+  onChangeImage( selector1, selector2){
+    this.image.flag = true;
+    $('#'+selector2)[0].src = window.URL.createObjectURL($('#'+selector1+' input')[0].files[0]);
+    console.log(window.URL.createObjectURL($('#'+selector1+' input')[0].files[0]));
+  }
+  onSubmit(){
+    if(!confirm("Confirma a alteração das fotos?")) return;
+    if($('#input-capa input')[0].files[0]){
+      var form;
+      form = new FormData();
+      form.append('CAPA_IMG', $('#input-capa input')[0].files[0])
+      $.ajax({
+        type: "POST",
+        url: this.origin+'/instaparts/upload.php?IMG=CAPA_IMG&CODIGO='+btoa(this.codiprod),
+        data: form,
+        processData: false,
+        contentType: false,
+        success: (data)=>{
+          console.log(data)
+        }
+      });
+    }
+    if($('#input-img2 input')[0].files[0]){
+      var form;
+      form = new FormData();
+      form.append('IMG2_IMG', $('#input-img2 input')[0].files[0])
+      $.ajax({
+        type: "POST",
+        url: this.origin+'/instaparts/upload.php?IMG=IMG2_IMG&CODIGO='+btoa(this.codiprod),
+        data: form,
+        processData: false,
+        contentType: false,
+        success: (data)=>{
+          console.log(data)
+        }
+      });
+    }
+    if($('#input-img3 input')[0].files[0]){
+      var form;
+      form = new FormData();
+      form.append('IMG3_IMG', $('#input-img3 input')[0].files[0])
+      $.ajax({
+        type: "POST",
+        url: this.origin+'/instaparts/upload.php?IMG=IMG3_IMG&CODIGO='+btoa(this.codiprod),
+        data: form,
+        processData: false,
+        contentType: false,
+        success: (data)=>{
+          console.log(data)
+        }
+      });
+    }
+    // var form;
+    // form = new FormData();
+    // if($('#input-capa input')[0].files[0]) form.append('CAPA_IMG', $('#input-capa input')[0].files[0]);
+    // if($('#input-img2 input')[0].files[0]) form.append('IMG2_IMG', $('#input-img2 input')[0].files[0]);
+    // if($('#input-img3 input')[0].files[0]) form.append('IMG3_IMG', $('#input-img3 input')[0].files[0]);
+    
+    // $.ajax({
+    //   type: "POST",
+    //   url: 'http://192.168.15.12/instaparts/upload.php',
+    //   data: form,
+    //   processData: false,
+    //   contentType: false,
+    //   success: (data)=>{
+    //     console.log(data)
+    //   }
+    //   // ,
+    //   // dataType: dataType
+    // });
 
-    $.get( "http://192.168.15.12/instaparts/oci.php?CODIPROD="+this.codiprod, ( data )=>{
+  }
+  clickedSearch(){
+    $.get( this.origin+"/instaparts/oci.php?CODIPROD="+this.codiprod, ( data )=>{
       data = JSON.parse(data);
       if(!(data.data[0])){
         this.prod.flag = false;
@@ -29,16 +112,6 @@ export class Tab1Page {
       this.prod.flag = true;
     });
 
-  }
-
-  clickedBarcode(){
-    alert('');
-  }
-  onSearchChange(event){
-    this.prod.flag = false;
-  }
-  selectfile(ident){
-    $("#input-"+ident+" input").trigger('click');
   }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import $ from "jquery";
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  public items:any;
+  public origin:any = origin;
 
-  constructor() {}
+  constructor() {
+    $.get( this.origin+"/instaparts/filedb.php", ( data )=>{
+      data = JSON.parse(data);
+      this.items = data;
+      console.log(data);
+    });
+    // http://192.168.15.12/instaparts/filedb.php
+  }
 
 }
